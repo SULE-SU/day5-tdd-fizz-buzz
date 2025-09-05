b62be5534b818eb4c579d2729f78b8235805c820
SULE <sule1@oocl.com>
2025-09-05 11:01:16 +0800

    feat: countOff 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_cinput_1() {
+        int order = 1;
+
+        String word = FizzBuzz.countOff(order);
+
+        assert word.equals("1");
+
+    }
+
+
 }
