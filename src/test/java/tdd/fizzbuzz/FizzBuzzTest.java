e0a77d2d1c02abb4a3af830e0b9e727cea29f653
SULE <sule1@oocl.com>
2025-09-05 11:05:55 +0800

    feat: countOff 2

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -2,15 +2,27 @@ package tdd.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
 
     @Test
-    public void should_return_1_when_cinput_1() {
+    public void should_return_1_when_input_1() {
         int order = 1;
 
         String word = FizzBuzz.countOff(order);
 
-        assert word.equals("1");
+        assertEquals("1" , word);
+
+    }
+
+    @Test
+    public void should_return_2_when_input_2() {
+        int order = 2;
+
+        String word = FizzBuzz.countOff(order);
+
+        assertEquals("2" , word);
 
     }
 
